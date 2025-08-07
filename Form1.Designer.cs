be942bca0766a6e077ffaad7889675710766d66e
Ahmet Özer <ahmetozeratu@gmail.com>
2025-08-07 13:29:10 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsBildirim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button2 = new Button();
+            notifyIcon1 = new NotifyIcon(components);
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Location = new Point(516, 393);
+            button2.Name = "button2";
+            button2.Size = new Size(143, 29);
+            button2.TabIndex = 0;
+            button2.Text = "Bildirim Gönder";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Tag = "";
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            notifyIcon1.BalloonTipClicked += notifyIcon1_BalloonTipClicked;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(478, 97);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(198, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(390, 175);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(398, 212);
+            textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(537, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Gönderen";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(554, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 20);
+            label2.TabIndex = 5;
+            label2.Text = "İçerik";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 25);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(378, 413);
+            dataGridView1.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 60000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private NotifyIcon notifyIcon1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using static WindowsBildirim.Form1;
+namespace WindowsBildirim
+{
+    public partial class Form1 : Form
+    {
+
+
+        List<Veri> veriKaynagi = new List<Veri>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        IFirebaseConfig fc = new FirebaseConfig()
+        {
+            // Project Settings => Service Accounts => Database Secrets => Secrets
+            AuthSecret = "8B0USPMhmouiXxJM8dKryPphpnZUvIm1PqsxCsg1",
+            BasePath = "https://bildirimtest-c7f3b-default-rtdb.firebaseio.com"
+        };
+
+        IFirebaseClient client;
+
+        private void ShowBalloonTip(string title, string content)
+        {
+            // Bildirim gönderme iþlemleri
+            notifyIcon1.BalloonTipTitle = title;
+            notifyIcon1.BalloonTipText = content;
+            //notifyIcon1.Icon = SystemIcons.Information; bu da yapýlabilir
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;//icon olmazsa çalýþmaz
+            notifyIcon1.ShowBalloonTip(0); // 5000 milisaniye (5 saniye) süreyle göster
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(fc);
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView1.Columns.Add("Gönderici", "Gönderen");
+                dataGridView1.Columns.Add("Ýçeriði", "Ýçerik");
+                dataGridView1.Columns.Add("Okunmadurumu", "Okundu mu?");
+                FirebaseResponse verial = client.Get(@"Bildirimtbl");
+                Dictionary<string, Veri> verieþleme = JsonConvert.DeserializeObject<Dictionary<string, Veri>>(verial.Body.ToString());
+                if (verieþleme != null)
+                {
+                    foreach (var item in verieþleme)
+                    {
+                        dataGridView1.Rows.Add(item.Value.Gönderen, item.Value.Ýçerik, item.Value.Okundu);
+
+                    }
+                }
+                timer1.Start();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Veri Tabanýna Baðlantý Saðlanamadý!");
+            }
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+
+            if (textBox1.Text != null && textBox2.Text != null && textBox1.Text != "" && textBox2.Text != "")
+            {
+                Veri veri = new Veri()
+                {
+                    Gönderen = textBox1.Text,
+                    Ýçerik = textBox2.Text,
+                    Okundu = "Okunmadý"
+                };
+                //veriKaynagi.Add(new Veri { Gönderen = textBox1.Text, Ýçerik = textBox2.Text, Okundu = "Okunmadý" });
+
+                var setet = client.Set("Bildirimtbl/" + textBox1.Text, veri);
+                MessageBox.Show("Veriler Eklendi");
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView1.Columns.Add("Gönderici", "Gönderen");
+                dataGridView1.Columns.Add("Ýçeriði", "Ýçerik");
+                dataGridView1.Columns.Add("Okunmadurumu", "Okundu mu?");
+                FirebaseResponse verial = client.Get(@"Bildirimtbl");
+                Dictionary<string, Veri> verieþleme = JsonConvert.DeserializeObject<Dictionary<string, Veri>>(verial.Body.ToString());
+                foreach (var item in verieþleme)
+                {
+                    dataGridView1.Rows.Add(item.Value.Gönderen, item.Value.Ýçerik, item.Value.Okundu);
+
+                }
+
+                ShowBalloonTip(textBox1.Text, textBox2.Text);
+            }
+            else
+            {
+                MessageBox.Show("Gönderici Veya Ýçerik Boþ Býrakýlamaz!", "Uyarý", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            if (e != null)
+            {
+                string balloonTitle = notifyIcon1.BalloonTipTitle;
+
+                // Güvenli bir veri yolu oluþturmak için geçerli bir veri yolu yapýsý oluþturun
+                string veriYolu = "Bildirimtbl/" + balloonTitle.Replace(".", "_").Replace("#", "_").Replace("$", "_").Replace("[", "_").Replace("]", "_");
+
+                FirebaseResponse veriAl = client.Get(veriYolu);
+
+                // Veriyi sadece bulunduðunda iþlem yapýn
+                if (veriAl.Body != "null")
+                {
+                    Veri secilenVeri = veriAl.ResultAs<Veri>();
+                    secilenVeri.Okundu = "Okundu";
+                    var setet = client.Update(veriYolu, secilenVeri);
+
+                    if (setet.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.Columns.Clear();
+                        dataGridView1.Columns.Add("Gönderici", "Gönderen");
+                        dataGridView1.Columns.Add("Ýçeriði", "Ýçerik");
+                        dataGridView1.Columns.Add("Okunmadurumu", "Okundu mu?");
+                        FirebaseResponse verial = client.Get(@"Bildirimtbl");
+                        Dictionary<string, Veri> verieþleme = JsonConvert.DeserializeObject<Dictionary<string, Veri>>(verial.Body.ToString());
+                        foreach (var item in verieþleme)
+                        {
+                            dataGridView1.Rows.Add(item.Value.Gönderen, item.Value.Ýçerik, item.Value.Okundu);
+
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bildirim Okundu olarak iþaretlenirken bir hata oluþtu.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Bildirim bulunamadý veya zaten okunmuþ.", "Uyarý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            FirebaseResponse verial = client.Get(@"Bildirimtbl");
+            Dictionary<string, Veri> veritimer = JsonConvert.DeserializeObject<Dictionary<string, Veri>>(verial.Body.ToString());
+            if (verial.Body != "null")
+            {
+                foreach (var item in veritimer)
+                {
+                    if (item.Value.Okundu == "Okunmadý")
+                    {
+                        ShowBalloonTip(item.Value.Gönderen.ToString(),item.Value.Ýçerik.ToString());
+                        await Task.Delay(10000);
+                    }
+                }
+            }
+        }
+    }
+    public class Veri
+    {
+        public string Gönderen { get; set; }
+        public string Ýçerik { get; set; }
+
+        public string Okundu { get; set; }
+    }
+}
